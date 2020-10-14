e652436eb3299c4e65904377cf7495df2aca4f9b
Sean Moon <smoon@freshbooks.com>
2020-10-14 18:35:51 -0400

    Initial Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use url::Url;
+
+/*
+  usage - rust-youtube-dl [url]
+*/
 fn main() {
-    println!("Hello, world!");
+    let _args: Vec<String> = env::args().collect();
+    let url_result = Url::parse(_args[1].as_str());
+    let parsed_url = match url_result {
+        Ok(url) => url,
+        Err(error) => panic!("Oops! Something went wrong: {:?}", error),
+    };
+
+    println!("Argument passed {}", parsed_url.host().unwrap());
 }
