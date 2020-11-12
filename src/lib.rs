c42b3844f94f0425fa10021d23f854a37622c7de
Sean Moon <smoon@freshbooks.com>
2020-11-11 22:05:18 -0500

    Add ytplayer config parsing function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use url::Url;
 use regex::Regex;
+use reqwest::blocking::{Client, Response};
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue, ACCEPT, CONTENT_TYPE, USER_AGENT};
 
 pub fn parse_url(url_str: &str) -> Url {
     let re = Regex::new(r"^.*(?:(?:youtu(?:\.be|be\.com)/|v/|vi/|u/w/|embed/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*").unwrap();
@@ -15,3 +17,38 @@ pub fn parse_url(url_str: &str) -> Url {
 
     parsed_url
 }
+
+pub fn download_webpage(url: &str) -> String{
+    let mut headers = HeaderMap::new();
+    headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"));
+
+    let client: Client = reqwest::blocking::Client::new();
+    let res = (match client.get(url)
+            .headers(headers)
+            .send() {
+                Ok(response) => Ok(response),
+                Err(error) => Err(error),
+            }).unwrap().text().unwrap(); 
+    res
+}
+
+pub fn get_ytplayer_config(webpage_contents: String) {
+    let patterns = [
+        r";ytplayer\.config\s*=\s*({.+?});ytplayer",
+        r";ytplayer\.config\s*=\s*({.+?});",
+    ];
+
+    let mut ytplayer_config = "";
+
+    for pattern in patterns.iter() {
+        let re = Regex::new(pattern).unwrap();
+        for captured in re.captures_iter(webpage_contents.as_str()) {
+            println!("{}", &captured[0]);
+            break;
+            //ytplayer_config = &captured[0];
+        }
+    }
+
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env;
-use rust_youtube_dl::parse_url;
+use rust_youtube_dl::{parse_url, download_webpage, get_ytplayer_config};
 /*
   usage - rust-youtube-dl [url]
 */
@@ -7,4 +7,6 @@ use rust_youtube_dl::parse_url;
 fn main() {
     let _args: Vec<String> = env::args().collect();
     let parsed_url = parse_url(_args[1].as_str());
+    let contents = download_webpage(parsed_url.as_str());
+    get_ytplayer_config(contents);
 }
